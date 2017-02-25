48ff66ce83b08a3c553edb7150049c4062f952a0
VPashkov <vad.pashkov@gmail.com>
2017-02-25 14:57:49 +0300

    Add add and complete functions

--- src/Todoer.hs
+++ src/Todoer.hs
@@ -1,3 +1,14 @@
-module Todoer(create) where
+module Todoer(create, add, complete) where
 
+import Data.List
+
+type TodoList = [String]
+
+create :: TodoList
 create = []
+
+add :: TodoList -> String -> TodoList
+add list title = title : list
+
+complete :: TodoList -> String -> TodoList
+complete list item = delete item list
--- test/TodoerSpec.hs
+++ test/TodoerSpec.hs
@@ -5,6 +5,15 @@ import Todoer
 
 spec :: Spec
 spec = do
+
   describe "create" $ do
     it "creates empty todo list" $ do
-      create `shouldBe` ([] :: [Int])
+      create `shouldSatisfy` null
+
+  describe "add" $ do
+    it "adds new item to the list" $ do
+      add [] "New item" `shouldBe` ["New item"]
+
+  describe "complete" $ do
+    it "removes item from the list" $ do
+      complete ["Item1", "Item2"] "Item1" `shouldBe` ["Item2"]
